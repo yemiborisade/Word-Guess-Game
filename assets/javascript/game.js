f42280799f9eef9df50dc1c37a8e0d0324b4a786
Akinyemi Borisade <yemiborisade@akinyemis-mbp.attlocal.net>
2019-10-26 16:35:16 -0400

    styling done to page and java moved over to js sheet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,93 @@
-<script type="text/javascript">
-
-
 // array of words
-var strangeWords = ["eleven", "hopper", "starcourt mall", "mad max", "demidog", "hawkins"];
+var strangeWords = ["eleven", "hopper", "lucas", "mad max", "demidog", "waffles", "hawkins"];
+
+startGame();
 
 //chooses word randomly
-let randNum = Math.floor(Math.random() * strangeWords.length);
-let rightWord = [];
-let wrongWord = [];
-let chosenWord = strangeWords[randNum];
-let underScore = [];
+function startGame () {
+console.log(wrongWord)
+document.getElementById("wrongGuess").innerHTML = "";
+
+var randNum = Math.floor(Math.random() * strangeWords.length);
+var rightWord = [];
+var wrongWord = [];
+var chosenWord = strangeWords[randNum];
+var underScore = [];
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var winsElem = document.getElementById("wins");
+var lossesElem = document.getElementById("losses");
+
 
 // dom manipulation
-let docUnderScore = document.getElementsByClassName('underscore');
-let docRightGuess = document.getElementsByClassName('rightGuess');
-let docWrongGuess = document.getElementsByClassName('wrongGuess');
+var docUnderScore = document.getElementsByClassName('underscores');
+var docRightGuess = document.getElementsByClassName('rightGuess');
+var docWrongGuess = document.getElementsByClassName('wrongGuess');
+document.getElementById("numGuesses").textContent = guessesLeft;
+
 
 // create underscores based on length of word
-let generateUnderscore = () => {
-    for (let i = 0; i < chosenWord.lentgh; i++) {
-        underScore.push("_");
+var generateUnderscore = function() {
+    for (let i = 0; i < chosenWord.length; i++) {
+        underScore.push("_ ");
     }
     return underScore;
 }
 
 // get users guess
 document.addEventListener('keypress', (event) => {
-    let keycode = event.keyCode;
-    let keyword = String.fromCharCode(keycode);
+    var keycode = event.keyCode;
+    var keyword = String.fromCharCode(keycode);
 
 // is users guess is right 
     if (chosenWord.indexOf(keyword) > -1) {
     // add to users guess is right
         rightWord.push(keyword);
-        
-    // replace underscore with righ letter
-        underScore[chosenWord.indexOf(keyword)] = keyword;
-            docUnderScore[0].innerHTML = underScore.join(' ');
-            RightGuess[0].innerHTML = rightWord;
+       
+    for (let i = 0; i < chosenWord.length; i++) {
+        if (chosenWord[i] === keyword)
+        underScore[i] = keyword
+    } 
+    // replace underscore with right letter
+        // underScore[chosenWord.indexOf(keyword)] = keyword;
+        document.getElementById("underscores").textContent = underScore.join(' ');
+        // document.getElementById("rightGuess").textContent = rightWord;
     // checks to see if user word matches guesses
         if(underScore.join('') == chosenWord) {
             alert('You Win');
-        } else wrongWord.push(keyword); 
-        docWrongGuess[0].innerHTML = wrongWord;
-    }    
-});
+            startGame();
+        }
+    }   
+        else {
+            wrongWord.push(keyword);
+            guessesLeft--;
+            // changes to HTML
+            document.getElementById("numGuesses").textContent = guessesLeft;
+            document.getElementsByClassName("wrongGuess").textContent = wrongWord;
+        }
 
-docUnderScore[0].innerHTML = generateUnderscore().join(' ');
+        if (wrongWord.indexOf(keyword) < 0) {
+            wrongWord.push(keyword);
+            document.getElementById("wrongGuess").textContent = wrongWord;
+        } else {
+            alert("Youve used this letter!")
+        }   console.log(underScore)
+        if (underScore.indexOf("_ ") === -1) {
+        console.log("wins")
+        wins++;
+      } else if (wrongWord === chosenWord) {
+        losses++;
+      }
+
+      winsElem.textContent = wins;
+      lossesElem.textContent = losses;
+      console.log(lossesElem)
+
+});
+// console.log(generateUnderscore().join(''));
+document.getElementById("underscores").textContent = generateUnderscore().join(' ')
+
+// Generate wins and losses counter on page 
+
+}
